cee29abce77f27264d0952da388fe3f611314f5a
tcotidiane <51900661+tcotidiane33@users.noreply.github.com>
2021-09-15 20:45:18 +0200

    update code #header #color

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// dark mode tools
 const darkMode = document.getElementById("dark-mode");
 
 darkMode.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
 
+// portfolio
 $(".image").click(function () {
   $(".image").removeClass("active");
   $(this).addClass("active");
 });
+
+// 
