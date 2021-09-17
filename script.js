b2f1e474db6c38e0ece2a64d77329905c9ae4973
tcotidiane <51900661+tcotidiane33@users.noreply.github.com>
2021-09-17 21:50:58 +0200

    when i use modal in Jquery JS

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-// dark mode tools
-const darkMode = document.getElementById("dark-mode");
+// // dark mode tools
+// const darkMode = document.getElementById("dark-mode");
 
-darkMode.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+// darkMode.addEventListener("change", () => {
+//   document.body.classList.toggle("dark");
+// });
+
+// // portfolio
+// $(".image").click(function () {
+//   $(".image").removeClass("active");
+//   $(this).addClass("active");
+// });
+
+// $("#img").on("click", function () {
+//   // console.log("hello world ");
+//   // alert("merci");
+// });
+//
+
+$("#img").on("click", function () {
+  $(".imagepreview").attr("src", $(this).find("img").attr("src"));
+  $("#imagepreview").modal("show");
 });
-
-// portfolio
-$(".image").click(function () {
-  $(".image").removeClass("active");
-  $(this).addClass("active");
-});
-
-
-// 
