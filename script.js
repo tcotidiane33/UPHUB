5c8a68c1c80d2b4956d83ae05a5ff23348140823
tcotidiane <51900661+tcotidiane33@users.noreply.github.com>
2021-09-20 18:18:17 +0200

    test-commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// // dark mode tools
-// const darkMode = document.getElementById("dark-mode");
+// dark mode tools
+const darkMode = document.getElementById("dark-mode");
 
-// darkMode.addEventListener("change", () => {
-//   document.body.classList.toggle("dark");
-// });
+darkMode.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
 
-// // portfolio
-// $(".image").click(function () {
-//   $(".image").removeClass("active");
-//   $(this).addClass("active");
-// });
+// portfolio
+$(".image").click(function () {
+  $(".image").removeClass("active");
+  $(this).addClass("active");
+});
 
 // $("#img").on("click", function () {
 //   // console.log("hello world ");
@@ -21,3 +21,5 @@ $("#img").on("click", function () {
   $(".imagepreview").attr("src", $(this).find("img").attr("src"));
   $("#imagepreview").modal("show");
 });
+// ajout d'un span a la fin du body
+$("<span/>").appendTo("body");
