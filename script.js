f815c9c5a07d7ab44b668a3a1fadcc71bfeed5c6
tcotidiane <51900661+tcotidiane33@users.noreply.github.com>
2021-09-21 13:53:28 +0200

    update css

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ $("#img").on("click", function () {
 });
 // ajout d'un span a la fin du body
 $("<span/>").appendTo("body");
+
+// jQeury pour cacher tous les elements de la page .
+// $("div").hide();
+//  Pour connaitre le nombre de div
+$("div").length();
+// pour renvoyer tout les balise(*)
+$("*");
+// pour renvoyer tous les div de class content
+$("div.content");
+// objets de class menu et d'identifiant content
+$(".menu , #content");
+
+// recherche de p contenant des object avec class header
+// et afficher le contenu de l'objects
+$("p").find("header").show();
+// ou $("selecteur", "context")
+$(".header", $("p")).show();
+// recupperer tous les elements et les extraires
+$("div").get(2);
+// équivalent
+$("div")[2];
+// recuperation d'un élement
+$("div").eq(2);
+// recupération d'un "élement à partir de la fin"
+$("div").eq(-2);
+// 
